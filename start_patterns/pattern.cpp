21ce81b35e35b50d7945966df10b9f2efc7233d4
Rishi <rishichaturvedi0012@gmail.com>
2022-08-27 21:53:21 +0530

    for loops pattern day 2

--- start_patterns/pattern.cpp
+++ start_patterns/pattern.cpp
@@ -75,11 +75,106 @@ void pattern6(){
         cout << "\n";
     }
 }
+void pattern7(){
+    int n = 5;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<(n-i-1);j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+}
+void pattern8(){
+    int n = 5;
+    int count=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << count;
+            if(count) count = 0;
+            else count = 1;
+        }
+        cout << "\n";
+    }
+}
+void pattern9(){
+    int n = 4;
+    for(int i=0;i<n;i++){
+        for(int j=1;j<i+1;j++){
+            cout << j;
+        }
+        for(int k=0;k<(n+1-2*i);k++){
+            cout << " ";
+        }
+        for(int j=i;j>=1;j--){
+            cout << j;
+        }
+        cout << "\n";
+    }
+}
+void pattern10(){
+    int count=1;
+    int n = 5;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<=i;j++){
+            cout << count;
+            count++;
+        }
+        cout << "\n";
+    }
+}
+void pattern11(){
+    int n = 5;
+    char c = 'A';
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << char(c + j);
+        }
+        cout << "\n";
+    }
+}
+void pattern12(){
+    int n = 5;
+    char c = 'A';
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << char(c + i);
+        }
+        cout << "\n";
+    }
+}
+void pattern13(){
+    int n = 5;
+    char c = 'A';
+    for(int i=0;i<n;i++){
+        for(int j=i;j>=0;j--){
+            cout << char(c + n - j);
+        }
+        cout << "\n";
+    }
+}
+void pattern14(){
+    
+}
 int main(){
     // pattern1();
     // pattern2();
     // pattern3();
     // pattern4();
     // pattern5();
-    pattern6();
+    // pattern6();
+    // pattern7();
+    // pattern8();
+    // pattern9();
+    // pattern10();
+    // pattern11();
+    // pattern12();
+    // pattern13();
+    pattern14();
 }
